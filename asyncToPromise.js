9021d3835f9c051db9f66b1a7bea721adf7ee1b6
byeonghyeon jeon <51815947+pensieve92@users.noreply.github.com>
2022-04-28 00:14:09 +0900

    await은 언제 멈추나요?
    await은 Promise가 resolve될 때까지 멈춰준다!!
    await 뒤의 함수가 Promise가 아니면 기달리지 않는다..
    
    Promise는 resolve가 호출 될때 .then에서 사용할 수 있따.
    
    > (추측) axios 요청의 응답이 왔을때 resolve된다고 생각해도 될 거 같다

--- asyncToPromise.js
+++ asyncToPromise.js
@@ -25,4 +25,39 @@ const b = Promise.resolve(a); // to-be
 // a = return 값
 // b = Promise
 
-// ************************************
+// ********************************************************
+// *************** 추가 ***********************************
+// ********************************************************
+// await은 Promise의 resolve한 값을 리턴해야지 멈춰준다!!
+// ********************************************************
+// ********************************************************
+// await 예시
+function delayP(ms){
+ return new Promise((resolve, reject) => {
+   setTimeout(resolve, ms);
+  });
+}
+
+async function a() {
+  try {
+    console.log('1')
+    await delayP(1000);
+    console.log('2')
+    await delayP(1000);
+    console.log('3')
+    await delayP(1000);    
+    console.log('4')
+  }catch(err) {
+    console.error(err);
+  }
+}
+
+// await은 Promise의 resolve한 값을 리턴해야지 멈춰준다!!
+async function a() {
+  const a = await 1;
+  const b = await Promise.resolve(1);  
+}
+
+
+
+
